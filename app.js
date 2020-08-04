829af309a11cdb64911ef09afc8f44ca11a411b2
Yesman-coder <yesmanshiva@gmail.com>
2020-08-04 18:23:31 +0000

    replaced static element  :hammer:

--- app.js
+++ app.js
@@ -4,9 +4,9 @@ let excuseGenerator = () => {
   let what =  ["ate my food", "stole my pants", "robbed my computer", "died", "got sick", "kidnapped me", "needed my help"];
   let when = ["a minute ago", "yesterday", "last night", "right before coming here", "just a moment ago", "a few hours ago", "the day before yesterday"];
 
-  let whoIndex = Math.floor(Math.random() * 7);
-  let whatIndex = Math.floor(Math.random() * 7)
-  let whenIndex = Math.floor(Math.random() * 7);
+  let whoIndex = Math.floor(Math.random() * who.length);
+  let whatIndex = Math.floor(Math.random() * what.length)
+  let whenIndex = Math.floor(Math.random() * when.length);
 
   return who[whoIndex] + ' ' + what[whatIndex] + ' ' + when[whenIndex];
 
